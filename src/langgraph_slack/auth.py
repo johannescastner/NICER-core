ca617d3274fecd238f45e47d3280e0f62adad8de
Johannes Castner <johannes@towardspeople.co.uk>
2025-03-17 09:55:57 +0000

    fixed auth.py to be able to run in development mode

--- src/langgraph_slack/auth.py
+++ src/langgraph_slack/auth.py
@@ -1,9 +1,16 @@
 from langgraph_sdk import Auth
 import logging
-
+import os
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+# Get the environment from OS variables (default to "PROD" if not set)
+environment = os.getenv("ENVIRONMENT", "PROD")
+print(f"Current environment: {environment}")
+
+"""async def authenticate(request, path, headers, method):
+    logger.info("authenticate function called")"""
 
 auth = Auth()
 
@@ -12,8 +19,10 @@ async def authenticate(request, path, headers, method):
     logger.info("authenticate function called")
     user_agent = headers.get(b"user-agent")
     logger.info(f"User-Agent: {user_agent}")
-    if user_agent and user_agent.startswith(b"Slackbot"):
+    if (user_agent and user_agent.startswith(b"Slackbot")):
         logger.info("Slackbot identified; authentication successful")
         return {"identity": "default-user", "permissions": ["read", "write"]}
     logger.warning("Authentication failed; User-Agent not recognized")
+    if environment == "DEV":
+        return {"identity": "default-user", "permissions": ["read", "write"]}
     return None
