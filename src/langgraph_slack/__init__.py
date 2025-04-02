05c03fea45da86e89399a00d461c07f1e26bbfa7
Johannes Castner <johannes@towardspeople.co.uk>
2025-04-02 12:28:13 +0100

    added some logging; still debugging

--- src/langgraph_slack/__init__.py
+++ src/langgraph_slack/__init__.py
@@ -5,8 +5,12 @@ if sys.version_info < (3, 12):
     import typing_extensions
     import typing
     typing.TypedDict = typing_extensions.TypedDict
-
+import typing
+import typing_extensions
+import pydantic
 import logging
+
+
 import dotenv
 
 dotenv.load_dotenv()
@@ -15,3 +19,9 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
+# Log versions of relevant libraries
+logging.info(f"typing.TypedDict = {typing.TypedDict}")
+logging.info(f"typing_extensions.TypedDict = {typing_extensions.TypedDict}")
+logging.info(f"Is typing.TypedDict patched? {typing.TypedDict is typing_extensions.TypedDict}")
+logging.info(f"typing-extensions version: {typing_extensions.__version__}")
+logging.info(f"pydantic version: {pydantic.VERSION}")
